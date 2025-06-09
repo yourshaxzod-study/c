38c2aa386f10a1fe5bcb930811cc56c685998db1
yourshaxzod <yourshaxzod@gmail.com>
2025-06-10 04:57:22 +0500

    feat: add comment.c file in 01_Basics

---
+++ 01_Basics/comment.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // This is a standard library header file that allows us to use the printf function.
+
+int main(){ // The main function is the entry point of the program.
+    printf("Hello, World!\n"); // This line prints "Hello, World!" followed by a new line to the console.
+    return 0; // The return statement indicates that the program has finished executing successfully.
+} // End of the program
