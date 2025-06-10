b663c1da64944cd8731ed651ed23b4b5469c8237
yourshaxzod <yourshaxzod@gmail.com>
2025-06-10 05:09:43 +0500

    fix: correct output text in comment.c

--- 01_Basics/comment.c
+++ 01_Basics/comment.c
@@ -1,6 +1,9 @@
 #include <stdio.h> // This is a standard library header file that allows us to use the printf function.
 
-int main(){ // The main function is the entry point of the program.
+int main()
+{                              // The main function is the entry point of the program.
     printf("Hello, World!\n"); // This line prints "Hello, World!" followed by a new line to the console.
-    return 0; // The return statement indicates that the program has finished executing successfully.
+    return 0;                  // The return statement indicates that the program has finished executing successfully.
 } // End of the program
+
+/* This is a multi-line comment. */
