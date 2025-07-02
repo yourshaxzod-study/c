895d3b770be363956341c4524e77adf7f3096724
yourshaxzod <yourshaxzod@gmail.com>
2025-07-02 16:37:33 +0500

    feat: add data types example

--- 01_basics/data_types.c
+++ 01_basics/data_types.c
@@ -2,15 +2,19 @@
 
 int main()
 {
-    int items = 5;
-    float const_per_item = 12.99;
-    float total_cost = items * const_per_item;
-    char currency = '$';
+    char c = 'X';
+    short s = 32000;
+    int i = 123456;
+    long l = 9876543210L;
+    float f = 3.14f;
+    double d = 2.71828;
 
-    printf("Total cost: %c%.2f\n", currency, total_cost);
-    printf("Items: %d\n", items);
-    printf("Cost per item: %.2f\n", const_per_item);
-    printf("Currency: %c\n", currency);
+    printf("char: %c\n", c);
+    printf("short: %hd\n", s);
+    printf("int: %d\n", i);
+    printf("long: %ld\n", l);
+    printf("float: %.2f\n", f);
+    printf("double: %.5f\n", d);
 
     return 0;
 }
