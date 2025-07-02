896fd03844e8347bae8c02bca428889d8cfa7817
yourshaxzod <yourshaxzod@gmail.com>
2025-07-02 17:00:11 +0500

    feat: add prime checker example

---
+++ 11_practice/prime_checker.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int isPrime(int n)
+{
+    int i;
+
+    if (n <= 1)
+        return 0;
+    if (n <= 3)
+        return 1;
+    if (n % 2 == 0 || n % 3 == 0)
+        return 0;
+
+    for (i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num))
+    {
+        printf("%d is a prime number\n", num);
+    }
+    else
+    {
+        printf("%d is not a prime number\n", num);
+    }
+
+    return 0;
+}
