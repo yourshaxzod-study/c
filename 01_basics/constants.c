05b4b4b252088f829d6267b00a1c8eac6439bd19
yourshaxzod <yourshaxzod@gmail.com>
2025-07-02 16:23:52 +0500

    feat: add constants example

---
+++ 01_basics/constants.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    const int id = 12;
+    const int age = 18;
+    const float fee = 85.15;
+    const char grade = 'B';
+
+    printf("ID: %d\n", id);
+    printf("Age: %d\n", age);
+    printf("Fee: %.2f\n", fee);
+    printf("Grade: %c\n", grade);
+    
+    return 0;
+}
