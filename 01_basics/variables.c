400ec3de44904f57d0417eba53ca90ae0a0e23e8
yourshaxzod <yourshaxzod@gmail.com>
2025-07-02 16:22:26 +0500

    feat: add variables example

--- 01_basics/variables.c
+++ 01_basics/variables.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int year = 2006;
-    float price = 18.8;
-    char name = 's';
+    int id = 12;
+    int age = 18;
+    float fee = 85.15;
+    char grade = 'B';
+
+    printf("ID: %d\n", id);
+    printf("Age: %d\n", age);
+    printf("Fee: %.2f\n", fee);
+    printf("Grade: %c\n", grade);
     
     return 0;
 }
