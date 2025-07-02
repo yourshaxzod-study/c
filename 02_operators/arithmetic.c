4a4548e7b8b43ebe02788879e7ec7e3116b7133c
yourshaxzod <yourshaxzod@gmail.com>
2025-07-02 16:39:19 +0500

    feat: add arithmetic example

---
+++ 02_operators/arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 15, b = 4;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("Addition: %d\n", a + b);
+    printf("Subtraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+    printf("Modulus: %d\n", a % b);
+
+    return 0;
+}
