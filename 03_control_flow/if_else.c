f9b57554e258ede5c2e831bafa321b8369bd98fb
yourshaxzod <yourshaxzod@gmail.com>
2025-07-02 16:42:49 +0500

    feat: add if else example

---
+++ 03_control_flow/if_else.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+    {
+        printf("Positive number\n");
+    }
+    else if (num < 0)
+    {
+        printf("Negative number\n");
+    }
+    else
+    {
+        printf("Zero\n");
+    }
+
+    return 0;
+}
