4eb1e866ad5906c178f6b17612aec8767a6f9853
yourshaxzod <yourshaxzod@gmail.com>
2025-07-02 16:38:25 +0500

    feat: add input output example

--- 01_basics/input_output.c
+++ 01_basics/input_output.c
@@ -2,10 +2,22 @@
 
 int main()
 {
+    char name[50];
     int age;
+    float height;
 
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter your age: ");
     scanf("%d", &age);
-    printf("Your age is: %d\n", age);
+
+    printf("Enter your height: ");
+    scanf("%f", &height);
+
+    printf("\nName: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
 
     return 0;
 }
